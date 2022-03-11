284161b238c208bb7c4c86ae3b9cdfb89f419963
MilaRossi <liudmila.rossi@gmail.com>
2022-03-11 10:06:20 +0100

    added tests for emptyBurger, incorrectBurger, noAuth

--- src/test/java/ru/praktikum/OrderTest.java
+++ src/test/java/ru/praktikum/OrderTest.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertTrue;
 
 public class OrderTest {
@@ -37,5 +38,59 @@ public class OrderTest {
         assertTrue(number > 0);
         System.out.println(number);
     }
+
+    @Test
+    @DisplayName("создать заказ с авторизацией и без ингредиентов")
+    public void createOrderWithAuthAndWithoutIngredients() {
+        // генерация данных пользователя
+        User user = new User();
+        JSONObject credentials = user.generateCredentials();
+        // создание пользователя
+        user.createUser(credentials);
+        // логин пользователя
+        Response responseLoginUser = user.loginUser(credentials);
+        // извлечение токена
+        String accessTokenWithBearer = responseLoginUser.then().extract().path("accessToken");
+        String accessToken = accessTokenWithBearer.split(" ")[1];
+        // создать заказ
+        Order order = new Order();
+        JSONObject ingredients = order.emptyBurger();
+        Response responseOrderCreate = given().header("Content-type", "application/json").auth().oauth2(accessToken)
+                .body(ingredients.toString()).when().post("/api/orders");
+        responseOrderCreate.then().assertThat().statusCode(400)
+                .and().assertThat().body("message", equalTo("Ingredient ids must be provided"));
+    }
+
+    @Test
+    @DisplayName("создать заказ с авторизацией и с неверным хешем ингредиентов")
+    public void createOrderWithAuthAndWithIncorrectIngredients() {
+        // генерация данных пользователя
+        User user = new User();
+        JSONObject credentials = user.generateCredentials();
+        // создание пользователя
+        user.createUser(credentials);
+        // логин пользователя
+        Response responseLoginUser = user.loginUser(credentials);
+        // извлечение токена
+        String accessTokenWithBearer = responseLoginUser.then().extract().path("accessToken");
+        String accessToken = accessTokenWithBearer.split(" ")[1];
+        // создать заказ
+        Order order = new Order();
+        JSONObject ingredients = order.incorrectBurger();
+        Response responseOrderCreate = given().header("Content-type", "application/json").auth().oauth2(accessToken)
+                .body(ingredients.toString()).when().post("/api/orders");
+        responseOrderCreate.then().assertThat().statusCode(500);
+    }
+
+    @Test
+    @DisplayName("создать заказ без авторизации")
+    public void createOrderWithoutAuth() {
+        Order order = new Order();
+        JSONObject ingredients = order.incorrectBurger();
+        Response responseOrderCreate = given().header("Content-type", "application/json").auth().oauth2("accessToken")
+                .body(ingredients.toString()).when().post("/api/orders");
+        responseOrderCreate.then().assertThat().statusCode(403);
+    }
+
 
 }
