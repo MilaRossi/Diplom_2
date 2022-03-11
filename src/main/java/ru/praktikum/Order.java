c266c1df941b5189c0bb5ce6fb89e1be8e634e19
MilaRossi <liudmila.rossi@gmail.com>
2022-03-11 09:33:13 +0100

    added step making burger

--- src/main/java/ru/praktikum/Order.java
+++ src/main/java/ru/praktikum/Order.java
@@ -1,4 +1,34 @@
 package ru.praktikum;
 
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+
 public class Order {
+    @Step("составить бургер")
+    // получить ингредиенты
+    public Response getIngredients() {
+        return given().get("/api/ingredients");
+    }
+    // вытащить массив данных
+    ArrayList<HashMap> ingredients = getIngredients().then().extract().path("data");
+    // первый ингредиент
+    String firstIngredientId = ingredients.get(0).get("_id").toString();
+    // второй ингредиент
+    String secondIngredientId = ingredients.get(1).get("_id").toString();
+    // создать массив ингредиентов для бургера
+    String[] burgerIngredients = {firstIngredientId, secondIngredientId};
+    // создать тело запроса
+    public JSONObject ingredientsForBurger() {
+        JSONObject json = new JSONObject();
+        json.put("ingredients", burgerIngredients);
+        return json;
+    }
+
+
 }
